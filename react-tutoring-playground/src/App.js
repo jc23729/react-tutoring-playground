16f00cea4143decc7abef8408aa62b247f191c65
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-15 23:11:10 -0400

    Update App.js

--- react-tutoring-playground/src/App.js
+++ react-tutoring-playground/src/App.js
@@ -1,29 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
   // so const b is destructred giving us the inital value
   //b can be any variable name or first value, second value setb gives us a function
   //useState(0) 0 is the initial value of this variable
 
-  let a = 12
-  const [b, setb] = useState(0)
+  let a = 12;
+  let ref = useRef(0);
+  const [b, setb] = useState(0);
 
   return (
     <div className="App">
       {a}
-      
-      <button onClick={() => {
-        a = 15
-        console.log(a)
-      }}>change value</button>
+
+      <button
+        onClick={() => {
+          // a = 15
+          a = a + 1;
+          console.log(a);
+        }}>
+        change value
+      </button>
 
       {b}
 
-      <button onClick={() => {
-        setb(b+1)
-      }}>change state</button>
+      <button
+        onClick={() => {
+          setb(b + 1);
+        }}>
+        change state
+      </button>
+
+      {ref.current}
     </div>
   );
 }
